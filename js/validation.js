0471ff8b3e193a459ba3d640fa797d24eada97bd
Chris Mits <chrismits88@gmail.com>
2022-03-25 22:15:36 +0200

    Added submit functionality

--- js/validation.js
+++ js/validation.js
@@ -1,12 +1,17 @@
 const form = document.getElementById('contact-form');
-const formInputFields = [form.name, form.email, form.phone, form.message];
-const toastElement = document.querySelector('.toast');
-const toastNotification = new bootstrap.Toast(toastElement);
+const formInputFields = [form.name, form.email, form.phone, form.message, form.checkbox];
+const toastError = document.querySelector('.toast-error');
+const toastSuccess = document.querySelector('.toast-success');
+const toastErrorNotification = new bootstrap.Toast(toastError);
+const toastSuccessNotification = new bootstrap.Toast(toastSuccess);
 
-let isNameValid = false;
-let isEmailValid = false;
-let isPhoneValid = false;
-let isMessageValid = false;
+const flags = {
+	isNameValid: false, 
+	isEmailValid: false, 
+	isPhoneValid: false, 
+	isMessageValid: false, 
+	isCheckboxValid: false
+};
 
 // A function to check if field is empty. 
 // If it is, remove ivalid class (for aesthetic purposes)
@@ -31,34 +36,42 @@ const regexCheck = (regexCondition, field) => {
 const validateField = field => {
 	if(field === form.name) {
 		const regexName = /^([a-zA-Zά-ωΑ-ώ]+\s)*[a-zA-Zά-ωΑ-ώ]{4,20}$/;
-		isNameValid = regexCheck(regexName, field);
+		flags.isNameValid = regexCheck(regexName, field);
 	} else if(field === form.email) {
-		const regexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{3})$/;
-		isEmailValid = regexCheck(regexEmail, field);
+		const regexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,3})$/;
+		flags.isEmailValid = regexCheck(regexEmail, field);
 	} else if(field === form.phone) {
 		const regexPhone = /^([+][3][0])?([6]{1})([9]{1})([0-9]{8})$/;
-		isPhoneValid = regexCheck(regexPhone, field);
+		flags.isPhoneValid = regexCheck(regexPhone, field);
 	} else if(field === form.message) {
-		const regexMessage = /^([a-zA-Zά-ωΑ-ώ,.?]+\s)*[a-zA-Zά-ωΑ-ώ,.?]{1,500}$/m;
-		isMessageValid = regexCheck(regexMessage, field);
+		const regexMessage = /^([a-zA-Zά-ωΑ-ώ,.?!;-]+\s)*[a-zA-Zά-ωΑ-ώ,.?!;-]{1,500}$/m;
+		flags.isMessageValid = regexCheck(regexMessage, field);
+	} else if(field === form.checkbox) {
+		if(form.checkbox.checked) {
+			form.checkbox.parentElement.classList.remove('invalid');
+			flags.isCheckboxValid = true;
+		} else {
+			form.checkbox.parentElement.classList.add('invalid');
+			flags.isCheckboxValid = false;
+		}
 	}
 };
 
 // A simple function that creates the current time and injects it in the toast notification
 const currentTime = () => {
-	const timeElement = document.querySelector('.time');
+	const timeElements = document.querySelectorAll('.time');
 	const date = new Date();
 	const hh = date.getHours();
 	const mm = date.getMinutes();
 	const ss = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
 	const time = `${hh}:${mm}:${ss}`;
-	timeElement.textContent = time;
+	timeElements.forEach(timeElement => timeElement.textContent = time);
 	
 	const liveTime = setTimeout(function(){ currentTime() }, 1000);
 };
 
+// A function that checks the fields for errors and logs related information to the user
 const reportFieldErrors = () => {
-	const flags = {isNameValid, isEmailValid, isPhoneValid, isMessageValid};
 	const errorCountEl = document.querySelector('.error-count');
 	const errorReportEl = document.querySelector('.errors');
 	errorReportEl.textContent = '';
@@ -80,31 +93,55 @@ const reportFieldErrors = () => {
 		errorReportEl.innerHTML += `<i class="icofont-exclamation-circle"></i> "Το μήνυμά σας..." <br>`;
 		errorCount++;
 	}
+	if(!flags.isCheckboxValid) {
+		errorReportEl.innerHTML += `<i class="icofont-exclamation-circle"></i> "Απαιτείται αποδοχή" <br>`;
+		errorCount++;
+	}
 	
 	errorCountEl.textContent = errorCount;
 };
+
 
 // SUBMIT FORM EVENT
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
-	console.log(isNameValid, isEmailValid, isPhoneValid, isMessageValid);
-	
-	if(isNameValid && isEmailValid && isPhoneValid && isMessageValid) {
-		console.log('✔️ Form ready to be submitted!');
+	if(flags.isNameValid && flags.isEmailValid && flags.isPhoneValid && flags.isMessageValid && flags.isCheckboxValid) {
+		fetch("https://formsubmit.co/ajax/sales@startpilates.gr", {
+			method: "POST",
+			headers: { 
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: JSON.stringify({
+				Όνομα: form.name.value,
+				Email: form.email.value,
+				Τηλέφωνο: form.phone.value,
+				Μήνυμα: form.message.value,
+				Προϊον: form.selection.value === 'Επιλέξτε προϊόν' ? 'Καμία επιλογή': form.selection.value
+			})
+		}).then(response => response.json())
+			.catch(error => console.log(error));
+		toastSuccessNotification.show();
+		form.reset();
+		for (const key in flags) {
+			flags[key] = false;
+		}
 	} else {
-		console.log('🔴 Something went wrong...');
 		formInputFields.forEach(inputField => validateField(inputField));
 		reportFieldErrors();
-		toastNotification.show();
+		toastErrorNotification.show();
 	}
 });
 
-
 // Adding the event listeners to the input fields
 for (const inputField of formInputFields) {
-	inputField.addEventListener('keyup', e => validateField(e.target));
-	inputField.addEventListener('blur', e => removeInvalidClasses(e));
+	if(inputField.classList.contains('form-control')) {
+		inputField.addEventListener('keyup', e => validateField(e.target));
+		inputField.addEventListener('blur', e => removeInvalidClasses(e));
+	} else {
+		inputField.addEventListener('click', e => validateField(e.target));
+	}
 }
 
 // Init timer of the toast notification
